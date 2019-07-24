1a7dabe231923727fcbc59415ca533c4177b0c24
odyksa <odyksa@myagent.com.ua>
2019-07-24 18:15:35 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function TodoItem() {
+    return (
+        <span>Drink delicious tea</span>
+    );
 }
 
-export default App;
+function TodoList() {
+    return (
+        <ul>
+            <li>
+                <TodoItem />
+            </li>
+            <li>
+                <TodoItem />
+            </li>
+            <li>
+                <TodoItem />
+            </li>
+        </ul>
+    );
+}
+
+export default TodoList;
