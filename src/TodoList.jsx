a6c5cf8d4faff9801a1bee65e37972a30e6bb935
odyksa <odyksa@myagent.com.ua>
2019-08-01 10:42:56 +0300

    creat todo-list start template

---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    render() {
+        return(
+            <div className="todo-list">
+                
+            </div>
+        );
+    }
+}
+
+export default TodoList;
